7c4b53867479e22c3d11820ad005b2a43493d7f0
Pool_Little <hansu@cisco.com>
2017-01-11 12:01:25 +0800

    Jan 12

---
+++ Edge.h
@@ -0,0 +1,25 @@
+//
+// Created by pool_little on 05/01/2017.
+//
+
+#ifndef ALGORITHM_CPP_EDGE_H
+#define ALGORITHM_CPP_EDGE_H
+
+#include <iostream>
+#include <cassert>
+using namespace std;
+//定义template代表权重的数值类型
+template <typename weight>
+class Edge{
+private:
+    int s_node;
+    int d_node;
+    weight w_value;
+public:
+    //gets
+
+    //constructor
+    //other
+    //operator overload for compare edges' cost
+};
+#endif //ALGORITHM_CPP_EDGE_H
